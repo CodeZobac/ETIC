f01fe38eba6ed9766589664d6158bef115523041
Afonso Caboz <127037346+CodeZobac@users.noreply.github.com>
2024-01-17 19:17:22 +0000

    Added programação 2 folder

---
+++ Programação-2/Aula-2/app.js
@@ -0,0 +1,17 @@
+const menuA = (x) => {
+  let num = 20;
+  return x + num;
+};
+
+let int = 10;
+
+console.log(menuA(int));
+
+const add_numbers = (a, b) => {
+  return a + b;
+};
+
+let a = 1;
+let b = 10;
+
+console.log(add_numbers(a, b));
