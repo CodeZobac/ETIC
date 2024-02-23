365c8276aef258bd4fae4e8d841b1129beb678ab
Afonso Caboz <afonso.caboz@gmail.com>
2024-02-23 21:03:32 +0000

    Commit exercício apagar diretórios

---
+++ Arquitetura-sistemas/Script-Apagar_Diretorios/Apagar_Diretorios.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+
+
+folder_path="/home/afonso/ETIC/Arquitetura-sistemas/Script-Apagar_Diretorios/script-sample-folder"
+
+
+if [ ! -d "$folder_path" ]; then
+  echo "O diretório '$folder_path' não existe."
+  exit 1
+fi
+
+if [ $# -gt 0 ]; then
+  echo "A apagar todos os diretórios dentro de '$folder_path'..."
+  find "$folder_path" -mindepth 1 -type d -exec rm -rf {} +
+else
+  echo "A apagar diretórios não vazios dentro de '$folder_path'..."
+
+  find "$folder_path" -mindepth 1 -type d ! -empty -exec rm -rf {} +
+fi
+
+echo "Operação concluída."
+
+
