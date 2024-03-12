907e3b2628bb43facfd79efcfb4a990d2f615366
Afonso Caboz <afonso.caboz@gmail.com>
2024-03-12 16:06:46 +0000

    Update: working simulation API

--- Arquitetura-sistemas/container-API/api/src/main.py
+++ Arquitetura-sistemas/container-API/api/src/main.py
@@ -1,27 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from router import api_router
 from uvicorn import run
+
 
 app = FastAPI()
 
+app.include_router(router=api_router)
 
-@app.get("/")
-def get_root():
-    """
-    Returns a JSON response with a message indicating that the API was called using the GET method.
-    """
-    return {"message": "Chamou API com o método GET"}
+origins = [
+    "http://localhost:8000",
+]
 
-
-@app.post("/")
-def post_root():
-    """
-    Handle POST requests to the root endpoint.
-
-    Returns:
-        dict: A dictionary with a message indicating that the API was called with the POST method.
-    """
-    return {"message": "Chamou API com o método POST"}
-
+app.add_middleware(
+    middleware_class=CORSMiddleware,
+    allow_origins=origins,
+    allow_methods=["GET", "POST"],
+)
 
 if __name__ == "__main__":
     run(app, host="0.0.0.0", port=8001)
