f0012e4c302db52f20a41f979d559f5ff570c64d
Afonso Caboz <afonso.caboz@gmail.com>
2024-03-13 14:03:36 +0000

    Minor aesthetic changes

--- Arquitetura-sistemas/container-portfolio/main.js
+++ Arquitetura-sistemas/container-portfolio/main.js
@@ -18,7 +18,59 @@ window.onload = async () => {
 
   gallery.showSlide();
 
-  document.querySelector(".about").onmouseover = about();
+  const about = () => {
+    const a = document.querySelector(".about");
+    const hr = document.createElement("hr");
+    hr.style.height = "5px";
+    hr.style.backgroundColor = "#01372b";
+    hr.style.borderRadius = "20px";
+    a.appendChild(hr);
+  };
+
+  const projects = () => {
+    const p = document.querySelector(".projects");
+    const hr = document.createElement("hr");
+    hr.style.height = "5px";
+    hr.style.backgroundColor = "#01372b";
+    hr.style.borderRadius = "20px";
+    p.appendChild(hr);
+  };
+
+  const contact = () => {
+    const c = document.querySelector(".contact");
+    const hr = document.createElement("hr");
+    hr.style.height = "5px";
+    hr.style.backgroundColor = "#01372b";
+    hr.style.borderRadius = "20px";
+    c.appendChild(hr);
+  };
+
+  document.querySelector(".about").onmouseover = () => {
+    about();
+  };
+
+  document.querySelector(".about").onmouseout = () => {
+    const hr = document.querySelector(".about hr");
+    if (hr) hr.remove();
+  };
+
+  document.querySelector(".projects").onmouseover = () => {
+    projects();
+  };
+
+  document.querySelector(".projects").onmouseout = () => {
+    const hr = document.querySelector(".projects hr");
+    if (hr) hr.remove();
+  };
+
+  document.querySelector(".contact").onmouseover = () => {
+    contact();
+  };
+
+  document.querySelector(".contact").onmouseout = () => {
+    const hr = document.querySelector(".contact hr");
+    if (hr) hr.remove();
+  };
 };
 
 /**
