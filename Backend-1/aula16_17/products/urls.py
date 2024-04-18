d5a3c3ca1b0a73fb9e194c912d5d1fed8bc5fe81
Afonso Caboz <afonso.caboz@gmail.com>
2024-04-18 22:01:57 +0100

    Added exercises from lessons

--- Backend-1/aula16_17/products/urls.py
+++ Backend-1/aula16_17/products/urls.py
@@ -1,12 +1,9 @@
 from django.urls import path
 from products.views import ListAllProducts, ProductDetailView
-
 from django.contrib.auth.decorators import login_required
-from posts.views import PostListView
 
 
 urlpatterns = [
-    # path("",ListAllProducts.as_view(), name="products"),
     path("", login_required(ListAllProducts.as_view()), name="products"),
     path("<slug>", login_required(ProductDetailView.as_view()), name="product_detail"),
 ]
