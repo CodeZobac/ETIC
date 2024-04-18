fd90b78c29f70a87252cc1fb4ce13076b8e372ad
Afonso Caboz <afonso.caboz@gmail.com>
2024-04-18 20:45:04 +0100

    Added exercises from lessons

--- Backend-1/aula16/posts/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-
-class LoginForm(forms.form):
-    username= forms.CharField(required=True)
-    passowrd = forms.CharField(widget=forms.PasswordInput, required=True)
--- Backend-1/aula16/products/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from products.views import ListAllProducts
-
-
-urlpatterns=[
-    path("all",ListAllProducts.as_view())
-]
