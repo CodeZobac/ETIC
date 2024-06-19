c725774cc9c753743a57a6821f149c716192ca11
Afonso Caboz <afonso.caboz@gmail.com>
2024-06-19 22:04:22 +0100

    Added delete functionality

--- Projeto-1/mainapp/admin.py
+++ Projeto-1/mainapp/admin.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 from .models import Folder, File
 # Register your models here.
 
-admin.site.register(Folder)
-admin.site.register(File)
+# admin.site.register(Folder)
+# admin.site.register(File)
